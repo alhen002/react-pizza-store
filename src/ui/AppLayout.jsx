4cd46e7165960368690da834d4fbd67548173246
Alexander Henting <a.henting@posteo.de>
2023-10-22 19:03:59 +0200

    setup loading  & error routes

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
