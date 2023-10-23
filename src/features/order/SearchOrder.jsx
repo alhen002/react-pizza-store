7c1f90c1a7bc7fbea22c33ec78a09d946fe64269
Alexander Henting <a.henting@posteo.de>
2023-10-23 19:21:11 +0200

    actions

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    navigate(`/order/${query}`);
+    setQuery("");
+
+    if (!query) return;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
